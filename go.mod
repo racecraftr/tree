7baed887ce1d0bb6f3195496786cc2f447ca913d
Avi Gupta <savim2020@gmail.com>
2023-06-11 22:23:00 -0400

    added color + better indentation.

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module tree
 
 go 1.20
 
-require github.com/TwiN/go-color v1.4.0 // indirect
+require github.com/TwiN/go-color v1.4.0
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io/fs"
 	"log"
@@ -23,26 +24,60 @@ var printColors = []colorPrint{
 }
 
 const depthPart = "│ "
+const depthEnd = "├─"
 
-var currentPath = "/Users/avi/code/github/racecraftr/gradzilla"
+var currentPath string
+
+var maxDepth int = 0xffff
 
 func main() {
+
+	flag.StringVar(&currentPath, "path", "", "path to look in.")
+	flag.IntVar(&maxDepth, "depth", 0xffff, "specifies the depth of the three. Must be greater than 0.")
+	flag.Parse()
+
+	if maxDepth < 0 {
+		maxDepth = 0xffff
+	}
+
+	if _, err := os.Stat(currentPath); err != nil {
+		currentPath, _ = os.Getwd()
+	}
+
 	fmt.Println("current path:\n" + currentPath)
+	fmt.Println(maxDepth)
 	err := filepath.Walk(currentPath, func(path string, info fs.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
 
 		slashIndex := strings.LastIndexByte(path, os.PathSeparator)
 		folderStr, filestr := path[:slashIndex], path[slashIndex:]
 
-		depth := len(strings.Split(folderStr, string(os.PathSeparator))) + 1
-		depth -= len(strings.Split(currentPath, string(os.PathSeparator)))
+		// depth := len(strings.Split(folderStr, string(os.PathSeparator))) + 1
+		// depth -= len(strings.Split(currentPath, string(os.PathSeparator)))
+		depth := 1
+		for _, c := range currentPath {
+			if c == os.PathSeparator {
+				depth --
+			}
+		}
+		for _, c := range folderStr {
+			if c == os.PathSeparator {
+				depth ++
+				if depth > maxDepth {
+					return nil
+				}
+			}
+		}
 
 		depthStr := ""
 		for i := 0; i < depth; i++ {
-			print(printColors[i % len(printColors)](depthPart))
+			if i == depth - 1 {
+				print(printColors[i % len(printColors)](depthEnd))
+			} else {
+				print(printColors[i % len(printColors)](depthPart))
+			}
+
 		}
+
 
 		str := fmt.Sprintf("%v%v", depthStr, filestr)
 
